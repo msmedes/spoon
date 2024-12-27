02aa7e9c15cc11461c3b54894c46135505fa043e
Michael Smedes <smichaelmedes@gmail.com>
2024-12-26 21:43:35 -0500

    tiny spoon

--- src/again.ts
+++ src/again.ts
@@ -7,31 +7,126 @@ type ExamplePaths = {
 	paths: Record<string, any>;
 };
 
-// type SplitPath<T extends string> = T extends `/${infer First}/${infer Rest}`
-// 	? [First, ...SplitPath<`${Rest}`>]
-// 	: T extends `${infer First}/${infer Rest}`
-// 		? [First, ...SplitPath<`${Rest}`>]
-// 		: T extends `/${infer Single}`
-// 			? Single extends ""
-// 				? []
-// 				: [Single]
-// 			: T extends `${infer Single}`
-// 				? Single extends ""
-// 					? []
-// 					: [Single]
-// 				: never;
+type Create<Spec extends ExamplePaths> = Spec extends {
+	paths: infer Schema extends Record<string, unknown>;
+}
+	? Prettify<Sign4<Schema>>
+	: "Fail";
 
-type Create<Spec extends ExamplePaths> = Prettify<Sign<Spec["paths"]>>;
+type Function = (...args: any[]) => any;
 
-type Sign<Route extends Record<string, any>> = {
-	[K in keyof Route as K extends `${infer First}/${infer Rest}`
-		? First
-		: K]: K extends `${infer First}/${infer Rest}`
-		? Prettify<Sign<{ [P in Rest]: Route[`${First}/${P}`] }>>
-		: Route[K];
+type PathParameter = `{${string}}`;
+
+type PathKey<Route extends Record<string, any>> = {
+	[K in keyof Route as K extends PathParameter
+		? (string & {}) | K
+		: K]: Route[K] extends Function ? Route[K] : PathKey<Route[K]>;
 };
 
-type Test = Sign<(typeof examplePaths)["paths"]>;
+type TestPath = PathKey<(typeof examplePaths)["paths"]>;
+
+/*
+Considering this object: {
+	paths: {
+	  "": { get: () => {}}
+		"store/inventory": { get: () => {} },
+		"store/order": { post: () => {}, put: () => {} },
+		"store/order/{orderId}": { get: () => {} },
+		"mirror": { get: () => {} },
+	},
+};
+
+We want a type that looks like this:
+type ApiClient = {
+  store: {
+	  order: {
+			post: () => {}
+			put: () => {}
+			orderId: {
+			  get: () => {}
+			}
+		}
+		inventory: {
+		  get: () => {}
+		}
+	}
+	mirror: {
+		get: () => {}
+	}
+	index: {
+		get: () => {}
+	}
+}
+
+So let's describe the type in plain english first:
+
+For each key in paths, we need to handle these cases:
+1) it is a path parameter, eg. `{someId}`
+2) It is a single segment path, eg. `store`
+3) It is a multi-segment path, eg `store/inventory`
+4) it is an HTTP method name, eg. get, post, put, patch
+
+Typescript defs for each here:
+1) if Path is a path parameter, the key should be that path parameter, 
+  and it should be equal to a recursion of this type on the Paths indexed by this key.
+2) if Path is just `${string}` then again we should just recurse this type on the Paths indexed on this Path
+3) if Path is multi-segment, then the key should be the First part of the segment, and the value would be the 
+   the recursed type on the remainder, (this part is a fail so far, might be better to make a tuple or something idk]
+4) If its an HTTP method name, there is no key, just a value.
+
+
+<Ignore>
+I think a tricky thing here is we kind of need the entire path at each level in the recursion??? otherwise
+we won't be able to index into the source object correctly.
+I'm not sure if I'm being tricky here but modeling this as a trie might be the move? I have no idea how
+to to this in the type system.
+
+Maybe the dumb guy move is to just literally make an object then typeof that over. 
+</Ignore>
+
+*/
+
+type HttpMethods = "get" | "post";
+
+type BaseSchema = Record<string, any>;
+
+type Split<T extends string> = T extends `${infer First}/${infer Rest}`
+	? [First, Rest]
+	: [T];
+
+type Sign4<
+	CurrentSchema extends BaseSchema,
+	CompletePath extends string = "",
+	CurrentPath extends string = "",
+> = CurrentPath extends `${string}/${string}`
+	? {
+			[K in Split<CurrentPath>[0]]: Sign4<
+				CurrentSchema,
+				CompletePath,
+				Split<CurrentPath>[1]
+			>;
+		}
+	: {
+			[K in keyof CurrentSchema as K extends `${infer First}/${string}`
+				? First
+				: K extends `${infer SinglePathSegment}`
+					? SinglePathSegment
+					: K extends `{${infer PathParameter}}`
+						? PathParameter
+						: K extends HttpMethods
+							? K
+							: K extends "/" | ""
+								? "index"
+								: never]: K extends `${string}/${infer Rest}`
+				? Sign4<CurrentSchema, CompletePath, Rest>
+				: K extends `${infer SinglePathSegment}`
+					? Sign4<CurrentSchema[SinglePathSegment], CompletePath>
+					: K extends `{$infer PathParameter}`
+						? Sign4<CurrentSchema[`{$PathParameter}`], CompletePath>
+						: CurrentSchema[K];
+		};
+
+type Test = Sign4<(typeof examplePaths)["paths"]>;
 
 const createProxy = (domain: string, path = "") => {
 	return new Proxy(() => {}, {
@@ -62,12 +157,13 @@ function createApiClient<T extends ExamplePaths>(domain: string): Create<T> {
 
 const examplePaths = {
 	paths: {
-		"/store/inventory": { get: () => {} },
-		"/store/order": { post: () => {} },
-		"/store/order/{orderId}": { get: () => {} },
+		"store/inventory": { get: () => {} },
+		"store/order": { post: () => {}, put: () => {} },
+		"store/order/{orderId}": { get: () => {} },
+		"{someId}": { get: () => {} },
+		mirror: { get: () => {} },
 	},
 };
 
 const client = createApiClient<typeof examplePaths>("localhost:8001");
-
-const cool = client.store.order;
+const cool = client.store.inventory;
